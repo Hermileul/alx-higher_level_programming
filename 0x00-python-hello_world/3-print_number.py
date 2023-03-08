0543cb1a483068677fdb5a1ef1fb46dd73831f43
hermileul <hermileul5@gmail.com>
2023-03-08 04:34:22 -0800

    3-print_number.py

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,31 +1,3 @@
-#include <stdio.h>
-#include "lists.h"
-
-/**
-* check_cycle - function that checks for if a singlt has a cycle
-*
-* @list: lined list to check
-*
-* Return: 1 if the list has a cycle, 0 if it doesn't
-*/
-
-int check_cycle(listint_t *list)
-
-{
-listint_t *slow = list;
-listint_t *fast = list;
-
-if (!list)
-return (0);
-
-while (slow && fast && ->next)
-{
-slow = slow->next;
-fast = fast ->next ->next;
-if (slow == fast)
-return (1);
-}
-
-return (0);
-}
-
+#!/usr/bin/python3
+number = 98
+print(f"{number:d} Battery street")
